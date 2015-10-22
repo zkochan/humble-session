61d488b84465e1b08e9e7e52527575a1e85007c5
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-10-22 23:48:00 +0300

    new test added.

--- test/index.js
+++ test/index.js
@@ -56,4 +56,68 @@ describe('humble-session', function() {
       done();
     });
   });
+
+  it('creates session', function(done) {
+    var server = new Hapi.Server();
+    server.connection();
+    server.register([{
+      register: require('simple-session-store')
+    }, {
+      register: humbleSession,
+      options: {
+        password: 'some-password',
+        sessionStoreName: 'simple-session-store',
+        ttl: 60 * 1000
+      }
+    }], function(err) {
+      server.route({
+        method: 'GET',
+        path: '/create-session',
+        handler: function(req, reply) {
+          var session = {
+            value: 'foo'
+          };
+          reply.setSession(session, function(err) {
+            expect(err).to.not.exist;
+            reply('ok');
+          });
+        }
+      });
+
+      server.route({
+        method: 'GET',
+        path: '/read-session',
+        config: {
+          pre: [humbleSession.pre],
+          handler: function(req, reply) {
+            expect(req.pre.session.value).to.eq('foo');
+
+            req.getSession(function(err, session) {
+              expect(err).to.not.exist;
+              expect(req.pre.session.value).to.eq('foo');
+
+              reply();
+              done();
+            });
+          }
+        }
+      });
+
+      server.inject('/create-session', function(res) {
+        var header = res.headers['set-cookie'];
+        expect(header.length).to.equal(1);
+        expect(header[0]).to.contain('sid=');
+        expect(header[0]).to.contain('Max-Age=60');
+        var cookie = header[0].match(/(?:[^\x00-\x20\(\)<>@\,;\:\\"\/\[\]\?\=\{\}\x7F]+)\s*=\s*(?:([^\x00-\x20\"\,\;\\\x7F]*))/);
+
+        server.inject({
+          url: '/read-session',
+          method: 'GET',
+          headers: {
+            cookie: 'sid=' + cookie[1]
+          }
+        });
+      });
+    });
+  });
 });
